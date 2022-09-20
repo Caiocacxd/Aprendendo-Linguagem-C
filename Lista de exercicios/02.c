12f19849e0440996097800e968519995338661ab
Caiocacxd <88864750+Caiocacxd@users.noreply.github.com>
2022-09-20 18:45:43 -0300

    matriz média alunos

--- Lista de exercicios/02.c
+++ Lista de exercicios/02.c
@@ -5,9 +5,9 @@
 int main() {
   setlocale(LC_ALL, "Portuguese");
 
-  float notasAlunos[3][3], media[10], soma;
+  float notasAlunos[10][3], media[10], soma;
 
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 10; i++) {
     printf("Digite as 3 notas do %dº aluno: \n", i + 1);
     soma = 0;
     for (int j = 0; j < 3; j++) {
@@ -19,18 +19,18 @@ int main() {
     printf("\n");
   }
 
-  for (int i = 0; i < 3; i++) {
-    printf("Notas do %dº aluno: ", i + 1);
+  for (int i = 0; i < 10; i++) {
+    printf("Notas do %dº aluno:", i + 1);
     for (int j = 0; j < 3; j++) {
-      printf("\t\t%.1f", notasAlunos[i][j]);
+      printf("\t%.1f", notasAlunos[i][j]);
     }
-    printf("\t\tMédia: %.1f", media[i]);
+    printf("\t Média: %.1f", media[i]);
 
-      if (media[i] >= 7) {
-        printf("\n(Aprovado)");
-      } else {
-        printf("\n(Reprovado!)");
-      }
+    if (media[i] >= 7) {
+      printf("\n(Aprovado)");
+    } else {
+      printf("\n(Reprovado!)");
+    }
     printf("\n\n");
   }
   return 0;
