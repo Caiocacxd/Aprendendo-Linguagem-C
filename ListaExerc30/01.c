5961408d3db24a499d157e2045b976742a56dd2b
Caiocacxd <88864750+Caiocacxd@users.noreply.github.com>
2022-10-01 21:34:20 -0300

    Create 01.c

---
+++ ListaExerc30/01.c
@@ -0,0 +1,29 @@
+Lógica Receita de Bolo
+Ingredientes:
+2 xícaras (chá) de açúcar
+3 xícaras (chá) de farinha de trigo
+4 colheres (sopa) de margarina
+3 ovos
+1 e 1/2 xícara (chá) de leite
+1 colher (sopa) bem cheia de fermento em pó
+
+Aparelhos:
+Forma grande de furo central
+3 xícaras
+3 colheres
+Batedeira
+
+1. Está com os ingredientes? Se sim (Passo 3) Se não (Passo 2)
+2. Pegar os ingredientes do bolo
+3. Está com os aparelhos de cozinha? Se sim (Passo 5) Se não (Passo 4)
+4. Pegar os aparelhos pro bolo
+5. Com a Batedeira, Bata as claras em neve e reserve
+6. Misture as gemas, a margarina e o açúcar até obter uma massa homogênea
+7. Acrescente o leite e a farinha de trigo aos poucos, sem parar de bater
+8. Por último, adicione as claras em neve e o fermento
+9. Despeje a massa em uma forma grande de furo central untada e enfarinhada
+10. Coloque a forma com a massa formada no forno
+10. Asse em forno médio 180 °C, preaquecido, por aproximadamente 40 minutos ou até furar o bolo com um garfo, este saia limpo
+11. Retire  do forno a forma com o bolo 
+12. Quando esfriar, transferir o bolo da forma para uma travessa de vidro
+13. Guarde o bolo em local de fácil acesso
