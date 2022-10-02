1a3628a7f4d7a62a8bc898b48c7464c533f8b7f5
Caiocacxd <88864750+Caiocacxd@users.noreply.github.com>
2022-10-01 21:35:01 -0300

    Create 02.c

---
+++ ListaExerc30/02.c
@@ -0,0 +1,9 @@
+Lógica Soma de dois números
+1. Inserir primeiro número
+2. Receber primeiro número
+3. Guarde o primeiro número
+4. Inserir segundo número
+5. Receber segundo número
+6. Guarde o segundo número
+7. Some os dois números
+8. Mostre o resultado da Soma
