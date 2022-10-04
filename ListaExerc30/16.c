ea552592dfc6731b3423ccec48da9fb23229cce1
Caiocacxd <88864750+Caiocacxd@users.noreply.github.com>
2022-10-03 22:59:00 -0300

    Update 16.c

--- ListaExerc30/16.c
+++ ListaExerc30/16.c
@@ -1,30 +1,33 @@
-#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
-int main() {
-  setlocale(LC_ALL, "Portuguese");
-  int i;
-  float valor[10], Maior, Menor;
-
-  printf("Digite 10 valores:\n");
-  printf("> ");
-  scanf("%f", &valor[0]);
-  Maior = valor[0];
-  Menor = valor[0];
+int main (){
+    setlocale(LC_ALL, "Portuguese");
   
-  for(int i = 1; i < 10; i++){
-    printf("> ");
-    scanf("%f", &valor[i]);
-    if(valor[i] > Maior){
-      Maior=valor[i];
-    }else if(valor[i] < Menor){
-      Menor=valor[i];
+    int var1, var2, var3;
+    
+    restart: printf("Digite três valores para os lados de um triângulo:\n");
+    scanf("%d %d %d", &var1, &var2, &var3);
+    if(var1 == 0 || var2 == 0 || var3 == 0){
+      printf("\n\nValor inválido! Digite outro valor...\n");
+      goto restart;
     }
-  }
 
-  printf("O menor valor é: %.1f", Menor);
-  printf("O maior valor é: %.1f", Maior);
-
-  return 0;
+    if(var1 < var2 + var3 && var2 < var1 + var3 && var3 < var1 + var2){
+    printf("\nEstes valores formam um triângulo:\n");
+     
+      if(var1 == var2  && var1 == var3){
+        printf("\nUm triângulo equilátero.");
+      }else if(var1 == var2 || var1 == var3 || var2 == var3){
+        printf("\nUm triângulo isósceles.");
+      }else if(var1 != var2 != var3){
+        printf("\nUm triângulo escaleno.");
+      }
+      
+    }else{
+    printf("\nEstes valores não formam um triângulo...\n");
+    }
+  
+ return 0;
 }
